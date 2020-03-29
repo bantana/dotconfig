1357c81f91b0a0d9ae5b377e3aecd91135787e07
bantana <bantana@gmail.com>
2020-03-29 20:28:14 +0800

    update

--- install/clangd.sh
+++ install/clangd.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
 if [[ $(uname -s) == "Linux" ]]; then
-    sudo apt -y install clang-9 clang-tools-9 clang-9-doc clang-9-examples clang-format-9 clang libc6-dbg 
-    sudo apt -y install valgrind
-    sudo apt-get install clangd-9
-    sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-9 100
+    if [[ $(lsb_release -cs) == "eoan" ]]; then
+        sudo apt -y install clang-9 clang-tools-9 clang-9-doc clang-9-examples clang-format-9 clang libc6-dbg
+        sudo apt -y install valgrind
+        sudo apt-get install clangd-9
+        sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-9 100
+    fi
+    if [[ $(lsb_release -cs) == "focal" ]]; then
+        sudo apt -y install clang-10 clang-tools-10 clang-10-doc clang-10-examples clang-format-10 clang libc6-dbg
+        sudo apt -y install valgrind
+        sudo apt-get install clangd-10
+        sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-10 100
+    fi
     exit 0
 fi
 if [[ $(uname -s) == "Darwin" ]]; then
