4abd4cd8cf58c55ff84d04ae164b02a4f6dfa14b
bantana <bantana@gmail.com>
2020-03-31 02:32:47 +0800

    update

--- install/node12.sh
+++ install/node12.sh
@@ -14,11 +14,11 @@ if [[ "$(uname -s)" == "Linux" ]]; then
     # check ubuntu
     if [[ "$(lsb_release -is)" == "Ubuntu" ]]; then
         echo ">>> Found Ubuntu OS"
-        curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-        # curl -sL install-node.now.sh/lts |sudo bash
+        # curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+        curl -sL install-node.now.sh/lts |sudo -E bash -
         # sudo apt update
         # sudo apt -y install gcc g++ make
-        sudo apt -y install nodejs
+        # sudo apt -y install nodejs
     fi
 fi
 if [[ "$(uname -s)" == "Darwin" ]]; then
