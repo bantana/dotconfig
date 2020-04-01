b2380cb5ecb62e6d78e6cf123c4a479306641437
bantana <bantana@gmail.com>
2020-04-01 22:40:04 +0800

    present.sh $(myip) 3999 blog.sh $(myip) 4000 ./contents/

--- bin/blog.sh
+++ bin/blog.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # GO111MODULE=off go get -u -v golang.org/x/blog
-if [[ $# -eq 3 ]]; then
-   echo "Usage: blog.sh 0.0.0.0 3999";
+if [[ "$#" -ne 3 ]]; then
+   echo "Usage: blog.sh 0.0.0.0 3999 contents/";
+   echo "$#"
    exit 1;
 fi
 
-blog -reload -http "$1":"$2" -template "$(go env GOPATH)/src/golang.org/x/blog/template" -static "$(go env GOPATH)/src/golang.org/x/blog/static" -website "$(go env GOPATH)/src/golang.org/x/website/content/static/" -content "./"
+blog -reload -http "$1":"$2" -template "$(go env GOPATH)/src/golang.org/x/blog/template" -static "$(go env GOPATH)/src/golang.org/x/blog/static" -website "$(go env GOPATH)/src/golang.org/x/website/content/static/" -content "$3"
--- bin/present.sh
+++ bin/present.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # GO111MODULE=off go get -u -v golang.org/x/tools/cmd/present
-if [[ $# -eq 3 ]]; then
+if [[ "$#" -ne 2 ]]; then
    echo "Usage: present.sh 0.0.0.0 3999";
+   echo "$#";
    exit 1;
 fi
 present -http "$1":"$2" -base "$(go env GOPATH)/src/golang.org/x/tools/cmd/present" -orighost "$1"
