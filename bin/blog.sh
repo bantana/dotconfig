9ee45a5483b5ef346f78ae9acb4fc3126d977d6b
bantana <bantana@gmail.com>
2020-04-02 18:50:06 +0800

    update

--- bin/blog.sh
+++ bin/blog.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # GO111MODULE=off go get -u -v golang.org/x/blog
+if [[ ! -f "$HOME/go/bin/blog" ]]; then
+    echo "not found $HOME/go/bin/blog. installing"
+    GO111MODULE=off go get -u -v golang.org/x/blog
+fi
 if [[ "$#" -ne 3 ]]; then
    echo "Usage: blog.sh 0.0.0.0 3999 contents/";
    echo "$#"
    exit 1;
 fi
-
 blog -reload -http "$1":"$2" -template "$(go env GOPATH)/src/golang.org/x/blog/template" -static "$(go env GOPATH)/src/golang.org/x/blog/static" -website "$(go env GOPATH)/src/golang.org/x/website/content/static/" -content "$3"
--- bin/present.sh
+++ bin/present.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 # GO111MODULE=off go get -u -v golang.org/x/tools/cmd/present
+if [[ ! -f "$HOME/go/bin/present" ]]; then
+    echo "not foun $HOME/go/bin/present. installing...."
+    GO111MODULE=off go get -u -v golang.org/x/tools/cmd/present
+fi
 if [[ "$#" -ne 2 ]]; then
    echo "Usage: present.sh 0.0.0.0 3999";
    echo "$#";
