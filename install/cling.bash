0d580b01dd413227848ef411b18de1dab65aaa9e
bantana <bantana@gmail.com>
2020-04-12 15:14:44 +0000

    The cling C++ interpreter cling.bash

--- install/cling.bash
+++ install/cling.bash
@@ -6,6 +6,16 @@ if [[ $(uname -s) == "Linux" ]]; then
 
 
     curl -sL https://root.cern.ch/download/cling/cling_2020-04-02_ubuntu18.tar.bz2 |tar  -xj --one-top-level="$HOME/cling" --strip-components 1
+
+echo "\
+The cling C++ interpreter
+https://root.cern.ch/cling
+
+To use cling, add the following line to your .profile:
+if [ -d "\$HOME/cling/bin" ] ; then
+    PATH="\$PATH:\$HOME/cling/bin"
+fi
+"
     exit 0
 fi
 if [[ $(uname -s) == "Darwin" ]]; then
