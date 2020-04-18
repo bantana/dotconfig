1482d747a24e5d4e08de1343763fd8f848339ccf
bantana <bantana@gmail.com>
2020-04-18 17:09:32 +0800

    update

--- install/rust.sh
+++ install/rust.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# git clone https://github.com/rust-analyzer/rust-analyzer.git ~/tmp/rust-analyzer && cd ~/tmp/rust-analyzer && cargo xtask install
-# cargo install cargo-watch
-# cargo install evcxr_repl
---
+++ install/rusttools.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+git clone https://github.com/rust-analyzer/rust-analyzer.git ~/tmp/rust-analyzer && cd ~/tmp/rust-analyzer && cargo xtask install
+cargo install cargo-watch
+cargo install evcxr_repl
