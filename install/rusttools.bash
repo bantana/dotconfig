495bebbec4e3ec220364324d1c8da14f5f3b18e2
bantana <bantana@gmail.com>
2020-04-21 09:25:06 +0800

    update

--- install/rusttools.bash
+++ install/rusttools.bash
@@ -2,3 +2,4 @@
 git clone https://github.com/rust-analyzer/rust-analyzer.git ~/tmp/rust-analyzer && cd ~/tmp/rust-analyzer && cargo xtask install
 cargo install cargo-watch
 cargo install evcxr_repl
+#cargo install crowbook
