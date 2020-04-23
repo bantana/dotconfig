9e603765bbec17d15dc06f21848bbd02bb13aea1
bantana <bantana@gmail.com>
2020-04-24 01:16:37 +0800

    update

--- install/rust-analyzer.bash
+++ install/rust-analyzer.bash
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 if [[ $(uname -s) == "Linux" ]]; then
+    echo "curl -sL https://github.com/rust-analyzer/rust-analyzer/releases/download/nightly/rust-analyzer-linux -o ~/.cargo/bin/rust-analyzer"
     curl -sL https://github.com/rust-analyzer/rust-analyzer/releases/download/nightly/rust-analyzer-linux -o ~/.cargo/bin/rust-analyzer
 fi
 if [[ $(uname -s) == "Darwin" ]]; then
+    echo "curl -sL https://github.com/rust-analyzer/rust-analyzer/releases/download/nightly/rust-analyzer-mac -o ~/.cargo/bin/rust-analyzer"
     curl -sL https://github.com/rust-analyzer/rust-analyzer/releases/download/nightly/rust-analyzer-mac -o ~/.cargo/bin/rust-analyzer
 fi
+if [[ $? -ne 0 ]]; then
+    echo "ERROR>>> error code: $?"
+    exit 1
+fi
+echo "success!"
