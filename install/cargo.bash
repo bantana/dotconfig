be963cedd8d5c317762dccc725a1bfff06833a24
bantana <bantana@gmail.com>
2020-04-25 18:07:29 +0800

    update

--- install/cargo.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cargo install cargo-watch
-cargo install evcxr_repl
-git clone https://github.com/rust-analyzer/rust-analyzer.git ~/tmp/rust-analyzer && cd ~/tmp/rust-analyzer && cargo xtask install
-
