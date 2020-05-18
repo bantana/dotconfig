ed8a7d8f0de49c3ad9ca5a2394f5bd2d6b544eee
bantana <bantana@gmail.com>
2020-05-18 23:47:05 +0800

    update

--- install/haskell_cabal_hls.bash
+++ install/haskell_cabal_hls.bash
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
-mkdir -p ~/tmp/haskell
-cd ~/tmp/haskell
+if [[ $(uname -s) == "Linux" ]]; then
+    mkdir -p ~/tmp/haskell
+    cd ~/tmp/haskell
 
-wget -c https://downloads.haskell.org/~cabal/cabal-install-3.2.0.0/cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz
-tar -xf cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz
-mv cabal /usr/local/bin/cabal
+    wget -c https://downloads.haskell.org/~cabal/cabal-install-3.2.0.0/cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz
+    tar -xf cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz
+    mv cabal /usr/local/bin/cabal
 
-wget -c https://downloads.haskell.org/~ghc/8.10.1/ghc-8.10.1-x86_64-deb10-linux.tar.xz
-tar -xf ghc-8.10.1-x86_64-deb10-linux.tar.xz
-cd ghc-8.10.1/
-./configure
-make
-make install
+    wget -c https://downloads.haskell.org/~ghc/8.10.1/ghc-8.10.1-x86_64-deb10-linux.tar.xz
+    tar -xf ghc-8.10.1-x86_64-deb10-linux.tar.xz
+    cd ghc-8.10.1/
+    ./configure
+    make
+    make install
 
-git clone git@github.com:haskell/haskell-language-server.git
-cd haskell-language-server
-./cabal-hls-install hls
+    git clone git@github.com:haskell/haskell-language-server.git
+    cd haskell-language-server
+    ./cabal-hls-install hls
 
 ## ~/.profile
 # # haskell cabal
 # if [[ -d "$HOME/.cabal/bin" ]]; then
 #   export PATH="$HOME/.cabal/bin:$PATH"
 # fi
+fi
