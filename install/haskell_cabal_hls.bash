e45fcbcf13f7c5b8a2c8072341f2c96709f47ce4
bantana <bantana@aozsky.com>
2020-05-19 15:22:19 +0800

    update

--- install/haskell_cabal_hls.bash
+++ install/haskell_cabal_hls.bash
@@ -3,31 +3,41 @@ if [[ $(uname -s) == "Linux" ]]; then
     mkdir -p ~/tmp/haskell
     cd ~/tmp/haskell
 
+    if [[ $(lsb_release -cs) == "bionic" ]]; then
+        sudo apt install libicu-dev libtinfo-dev libgmp-dev
+        wget -c http://ftp.br.debian.org/debian/pool/main/n/ncurses/libtinfo6_6.2-1_amd64.deb
+        sudo dpkg -i libtinfo6_6.2-1_amd64.deb
+    fi
+
     wget -c https://downloads.haskell.org/~cabal/cabal-install-3.2.0.0/cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz
     tar -xf cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz
-    mv cabal /usr/local/bin/cabal
-
-    ## forgot ?
-    # cabal update
+    sudo mv cabal /usr/local/bin/cabal
 
     wget -c https://downloads.haskell.org/~ghc/8.10.1/ghc-8.10.1-x86_64-deb10-linux.tar.xz
     tar -xf ghc-8.10.1-x86_64-deb10-linux.tar.xz
     cd ghc-8.10.1/
     ./configure
     make
-    make install
+    sudo make install
 
-    # in china
-    sed -i 's/repository hackage.haskell.org/repository mirrors.tuna.tsinghua.edu.cn/' ~/.cabal/config
-    sed -i 's/url: http:\/\/hackage.haskell.org/url: http:\/\/mirrors.tuna.tsinghua.edu.cn\/hackage/' ~/.cabal/config
-    sed -i 's/  -- secure: True/--  -- secure: True/' ~/.cabal/config
-    sed -i 's/  -- root-keys:/--  -- root-keys:/' ~/.cabal/config
-    sed -i 's/  -- key-threshold: 3/--  -- key-threshold: 3/' ~/.cabal/config
+    if [[ ! -z "$HTTPS_PROXY" ]]; then
+        # todo: check ip on china
+        echo "HTTPS_PROXY founded, try modify ~/.cabal/config repository " 
+        # generate ~/.cabal/config
+        cabal user-config init 
+        sed -i 's/repository hackage.haskell.org/repository mirrors.tuna.tsinghua.edu.cn/' ~/.cabal/config
+        sed -i 's/url: http:\/\/hackage.haskell.org/url: http:\/\/mirrors.tuna.tsinghua.edu.cn\/hackage/' ~/.cabal/config
+        sed -i 's/  -- secure: True/  -- secure: False/' ~/.cabal/config
+        sed -i 's/  -- root-keys:/  -- root-keys:/' ~/.cabal/config
+        sed -i 's/  -- key-threshold: 3/  -- key-threshold:/' ~/.cabal/config
+    fi
 
-    mkdir -p cabal_update
+    [[ ! -d "cabal_update" ]] && mkdir -p cabal_update
     cd cabal_update
+    echo "cabal update ..."
     cabal update
     cd ..
+    [[ -d "./cabal_update" ]] && rm -rf cabal_update
 
     git clone git@github.com:haskell/haskell-language-server.git
     cd haskell-language-server
@@ -36,12 +46,17 @@ fi
 if [[ $(uname -s) == "Darwin" ]]; then
     brew install ghc cabal-install
 
-    # in china
-    sed -i '' -e 's/repository hackage.haskell.org/repository mirrors.tuna.tsinghua.edu.cn/' ~/.cabal/config
-    sed -i '' -e 's/url: http:\/\/hackage.haskell.org/url: http:\/\/mirrors.tuna.tsinghua.edu.cn\/hackage/' ~/.cabal/config
-    sed -i '' -e 's/  -- secure: True/--  -- secure: True/' ~/.cabal/config
-    sed -i '' -e 's/  -- root-keys:/--  -- root-keys:/' ~/.cabal/config
-    sed -i '' -e 's/  -- key-threshold: 3/--  -- key-threshold: 3/' ~/.cabal/config
+    if [[ ! -z "$HTTPS_PROXY" ]]; then
+        # todo: check ip on china
+        echo "HTTPS_PROXY founded, try modify ~/.cabal/config repository " 
+        # generate ~/.cabal/config
+        cabal user-config init 
+        sed -i 's/repository hackage.haskell.org/repository mirrors.tuna.tsinghua.edu.cn/' ~/.cabal/config
+        sed -i 's/url: http:\/\/hackage.haskell.org/url: http:\/\/mirrors.tuna.tsinghua.edu.cn\/hackage/' ~/.cabal/config
+        sed -i 's/  -- secure: True/  -- secure: False/' ~/.cabal/config
+        sed -i 's/  -- root-keys:/  -- root-keys:/' ~/.cabal/config
+        sed -i 's/  -- key-threshold: 3/  -- key-threshold:/' ~/.cabal/config
+    fi
 
     mkdir -p ~/tmp/haskell
     cd ~/tmp/haskell
