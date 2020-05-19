c87564b4535f67888a0ac625d519399706ed63d5
bantana <bantana@aozsky.com>
2020-05-19 15:44:18 +0800

    update

--- install/haskell_cabal_hls.bash
+++ install/haskell_cabal_hls.bash
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+function checkproxy() {
+    if [[ ! -z "$HTTPS_PROXY" ]]; then
+        # todo: check ip on china
+        echo "HTTPS_PROXY founded, try modify ~/.cabal/config repository " 
+        # generate ~/.cabal/config
+        cabal user-config init 
+        sed -i 's/repository hackage.haskell.org/repository mirrors.tuna.tsinghua.edu.cn/' ~/.cabal/config
+        sed -i 's/url: http:\/\/hackage.haskell.org/url: http:\/\/mirrors.tuna.tsinghua.edu.cn\/hackage/' ~/.cabal/config
+        sed -i 's/  -- secure: True/  -- secure: False/' ~/.cabal/config
+        sed -i 's/  -- root-keys:/  -- root-keys:/' ~/.cabal/config
+        sed -i 's/  -- key-threshold: 3/  -- key-threshold:/' ~/.cabal/config
+    fi
+}
+
 if [[ $(uname -s) == "Linux" ]]; then
     mkdir -p ~/tmp/haskell
     cd ~/tmp/haskell
@@ -20,17 +34,7 @@ if [[ $(uname -s) == "Linux" ]]; then
     make
     sudo make install
 
-    if [[ ! -z "$HTTPS_PROXY" ]]; then
-        # todo: check ip on china
-        echo "HTTPS_PROXY founded, try modify ~/.cabal/config repository " 
-        # generate ~/.cabal/config
-        cabal user-config init 
-        sed -i 's/repository hackage.haskell.org/repository mirrors.tuna.tsinghua.edu.cn/' ~/.cabal/config
-        sed -i 's/url: http:\/\/hackage.haskell.org/url: http:\/\/mirrors.tuna.tsinghua.edu.cn\/hackage/' ~/.cabal/config
-        sed -i 's/  -- secure: True/  -- secure: False/' ~/.cabal/config
-        sed -i 's/  -- root-keys:/  -- root-keys:/' ~/.cabal/config
-        sed -i 's/  -- key-threshold: 3/  -- key-threshold:/' ~/.cabal/config
-    fi
+    checkproxy
 
     [[ ! -d "cabal_update" ]] && mkdir -p cabal_update
     cd cabal_update
@@ -46,17 +50,7 @@ fi
 if [[ $(uname -s) == "Darwin" ]]; then
     brew install ghc cabal-install
 
-    if [[ ! -z "$HTTPS_PROXY" ]]; then
-        # todo: check ip on china
-        echo "HTTPS_PROXY founded, try modify ~/.cabal/config repository " 
-        # generate ~/.cabal/config
-        cabal user-config init 
-        sed -i 's/repository hackage.haskell.org/repository mirrors.tuna.tsinghua.edu.cn/' ~/.cabal/config
-        sed -i 's/url: http:\/\/hackage.haskell.org/url: http:\/\/mirrors.tuna.tsinghua.edu.cn\/hackage/' ~/.cabal/config
-        sed -i 's/  -- secure: True/  -- secure: False/' ~/.cabal/config
-        sed -i 's/  -- root-keys:/  -- root-keys:/' ~/.cabal/config
-        sed -i 's/  -- key-threshold: 3/  -- key-threshold:/' ~/.cabal/config
-    fi
+    checkproxy
 
     mkdir -p ~/tmp/haskell
     cd ~/tmp/haskell
